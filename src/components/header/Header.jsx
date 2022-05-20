ce1aabb067e2e34d2b16dbb5324541623e36e2bf
ashishKumarDev123 <29ashishkumar@gmail.com>
2022-05-20 16:30:38 +0530

    about-container

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import './Header.css'
+import headerLogo from '../../assets/images/headerLogo.png'
+
+const Header = () => {
+
+  const [scrolled,setScroll] = useState(false);
+
+  useEffect(()=>{
+
+    window.addEventListener('scroll',scrollCheck)
+   
+  },[])
+
+  const scrollCheck=(e)=>{
+      let scrollTop =window.scrollY;
+      scrollTop > 50 ?  setScroll(true): setScroll(false);
+  }
+  return (
+    <div className={`header text-color-white position-fixed top-0 row d-flex justify-content-between ${scrolled && "backgroundBlack"}  `}>
+        <div className=' col-2 d-flex align-items-center'><img className='header-logo' src={headerLogo} alt="header logo"/></div>
+        <div className='nav-items-container text-color-white  container-fluid col-10 d-flex justify-content-end align-items-center'>
+                <div className=' nav-item'>Home</div>
+                <div className=' nav-item'>About</div>
+                <div className=' nav-item'>Projects</div>
+                <div className=' nav-item'>Partners</div>
+                <div className=' nav-item'>Contact</div>
+                <div className=' nav-item'>Claim</div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
