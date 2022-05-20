8436faac3c9810045881208bda40789f6ede3054
Sachinsharma7398 <sachin@nexgsolution.com>
2022-05-21 00:15:57 +0530

    launch section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import './App.css'
-import About from './components/about/About'
-import Banner from './components/banner/Banner'
-import Header from './components/header/Header'
+import React from "react";
+import "./App.css";
+import About from "./components/about/About";
+import Banner from "./components/banner/Banner";
+import Header from "./components/header/Header";
+import Launch from "./components/launch/Launch";
 
 const App = () => {
   return (
-    <div className='main-app-container'>
-      <Header/>
-      <Banner/>
+    <div className="main-app-container">
+      <Header />
+      <Banner />
       <About />
-
-      
+      <Launch />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/launch/Launch.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Launch.css'
+
+const Launch = () => {
+  return (
+    <div className='launch-container'>
+        <div className='launch-heading text-color-white'>LOOKING TO LAUNCH YOUR PROJECT?</div>
+        <div className='launch-para text-color-white'>We can offer you some of the greatest incubation services along with links to some of the strongest names in the industry.</div>
+        <div role="button" className='launch-button'>lets talk</div>
+    </div>
+  )
+}
+
+export default Launch
