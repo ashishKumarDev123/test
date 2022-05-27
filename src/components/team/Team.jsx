5ca1d548b1a453af9e4fa8f72067a1d55698652d
Sachinsharma7398 <sachin@nexgsolution.com>
2022-05-27 10:31:59 +0530

    card change

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -5,48 +5,107 @@ import RedCoin from "../../assets/images/redCoin.png";
 import Luna from "../../assets/images/luna.png";
 import portfolioItemImage from "../../assets/images/portfolioItem.jpeg";
 import team1 from "../../assets/images/team1.jpg";
+import up from "../../assets/images/up.jpg";
+import girl from "../../assets/images/girl.jpg";
+import long from "../../assets/images/long.jpg";
+import { FaTelegramPlane } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Team = () => {
   return (
     <div className="perfomers-section">
       <div className="perfomers-heading text-color-white">Our team</div>
       <div className="perfomers-items-container d-flex   text-color-white">
-
         <div className="teamItem">
+          <div className="teamItemImg">
           <img src={team1} alt="text" />
-          <h2>Rahul sharma</h2>
-          <h2>Manager</h2>
+          </div>
+          <h2 className="name">Rahul sharma</h2>
+          <h2 className="post">Manager</h2>
+          <div className="tele">
+            <FaTelegramPlane className="teleIcon icon" size={28} color="#ffffff" />
+            <MdEmail size={28} className="icon" color="#ffffff" />
+          </div>
         </div>
         <div className="teamItem">
-          <img src={team1} alt="text" />
-          <h2>Rahul sharma</h2>
-          <h2>Manager</h2>
+          <div className="teamItemImg">
+          <img src={up} alt="text" />
+          </div>
+          <h2 className="name">emran</h2>
+          <h2 className="post">Manager</h2>
+          <div className="tele">
+            <FaTelegramPlane className="teleIcon" size={28} color="#ffffff" />
+            <MdEmail size={28} color="#ffffff" />
+          </div>
         </div>
         <div className="teamItem">
-          <img src={team1} alt="text" />
-          <h2>Rahul sharma</h2>
-          <h2>Manager</h2>
+          <div className="teamItemImg">
+          <img src={long} alt="text" />
+          </div>
+          <h2 className="name">katty</h2>
+          <h2 className="post">Manager</h2>
+          <div className="tele">
+            <FaTelegramPlane className="teleIcon" size={28} color="#ffffff" />
+            <MdEmail size={28} color="#ffffff" />
+          </div>
         </div>
         <div className="teamItem">
-          <img src={team1} alt="text" />
-          <h2>Rahul sharma</h2>
-          <h2>Manager</h2>
+          <div className="teamItemImg">
+          <img src={girl} alt="text" />
+          </div>
+          <h2 className="name">julia</h2>
+          <h2 className="post">Manager</h2>
+          <div className="tele">
+            <FaTelegramPlane className="teleIcon" size={28} color="#ffffff" />
+            <MdEmail size={28} color="#ffffff" />
+          </div>
         </div>
         <div className="teamItem">
+          <div className="teamItemImg">
           <img src={team1} alt="text" />
-          <h2>Rahul sharma</h2>
-          <h2>Manager</h2>
+          </div>
+          <h2 className="name">Rahul sharma</h2>
+          <h2 className="post">Manager</h2>
+          <div className="tele">
+            <FaTelegramPlane className="teleIcon icon" size={28} color="#ffffff" />
+            <MdEmail size={28} className="icon" color="#ffffff" />
+          </div>
         </div>
         <div className="teamItem">
-          <img src={team1} alt="text" />
-          <h2>Rahul sharma</h2>
-          <h2>Manager</h2>
+          <div className="teamItemImg">
+          <img src={up} alt="text" />
+          </div>
+          <h2 className="name">Rahul sharma</h2>
+          <h2 className="post">Manager</h2>
+          <div className="tele">
+            <FaTelegramPlane className="teleIcon" size={28} color="#ffffff" />
+            <MdEmail size={28} color="#ffffff" />
+          </div>
         </div>
         <div className="teamItem">
-          <img src={team1} alt="text" />
-          <h2>Rahul sharma</h2>
-          <h2>Manager</h2>
+          <div className="teamItemImg">
+          <img src={long} alt="text" />
+          </div>
+          <h2 className="name">Rahul sharma</h2>
+          <h2 className="post">Manager</h2>
+          <div className="tele">
+            <FaTelegramPlane className="teleIcon" size={28} color="#ffffff" />
+            <MdEmail size={28} color="#ffffff" />
+          </div>
         </div>
+        <div className="teamItem">
+          <div className="teamItemImg">
+          <img src={girl} alt="text" />
+          </div>
+          <h2 className="name">Rahul sharma</h2>
+          <h2 className="post">Manager</h2>
+          <div className="tele">
+            <FaTelegramPlane className="teleIcon" size={28} color="#ffffff" />
+            <MdEmail size={28} color="#ffffff" />
+          </div>
+        </div>
+     
+      
        
       </div>
     </div>
